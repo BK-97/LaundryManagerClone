358340719c34cbfb99fae2227fac20595794dc7c
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-08-18 15:11:55 +0300

    Update

---
+++ Assets/[GameFolders]/Scripts/Managers/ExchangeManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum CurrencyType //Must be the same name as in PlayerPrefKeys
+{
+    Coin,
+    Cash
+}
+
+public class ExchangeManager : Singleton<ExchangeManager>
+{
+    private Dictionary<CurrencyType, int> currencyDictionary;
+    public DictonaryEvent OnCurrencyChange = new DictonaryEvent();
+    [HideInInspector]
+    public UnityEvent OnCurrencyAdded = new UnityEvent();
+    [HideInInspector]
+    public Vector3 UIPos;
+    public ExchangeManager()
+    {
+        currencyDictionary = new Dictionary<CurrencyType, int>();
+    }
+    private void Start()
+    {
+        currencyDictionary[CurrencyType.Cash] = PlayerPrefs.GetInt(PlayerPrefKeys.CurrentCash, 300); //For Test
+    }
+    public void SetUIPos(Vector3 pos)
+    {
+        UIPos = pos;
+    }
+
+    #region CurrencyMethods
+    public bool UseCurrency(CurrencyType currencyType, int amount)
+    {
+        if (currencyDictionary.ContainsKey(currencyType))
+        {
+            if (currencyDictionary[currencyType] >= amount)
+            {
+                currencyDictionary[currencyType] -= amount;
+                string exchangeName = currencyType.ToString();
+                PlayerPrefs.SetInt(exchangeName, currencyDictionary[currencyType]);
+                OnCurrencyChange.Invoke(currencyDictionary);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void AddCurrency(CurrencyType currencyType, int amount)
+    {
+        if (currencyDictionary.ContainsKey(currencyType))
+        {
+            currencyDictionary[currencyType] += amount;
+            string exchangeName = currencyType.ToString();
+            PlayerPrefs.SetInt(exchangeName, currencyDictionary[currencyType]);
+            OnCurrencyChange.Invoke(currencyDictionary);
+            OnCurrencyAdded.Invoke();
+        }
+    }
+
+    public int GetCurrency(CurrencyType currencyType)
+    {
+        if (currencyDictionary.ContainsKey(currencyType))
+        {
+            return currencyDictionary[currencyType];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    #endregion
+}
+public class DictonaryEvent : UnityEvent<Dictionary<CurrencyType, int>> { }
--- Assets/[GameFolders]/Scripts/Utilities/PlayerPrefKeys.cs
+++ Assets/[GameFolders]/Scripts/Utilities/PlayerPrefKeys.cs
@@ -3,4 +3,6 @@ public static class PlayerPrefKeys
     public static readonly string LastLevel = "LastLevel";
     public static readonly string FakeLevel = "FakeLevel";
     public static readonly string IsLooping = "IsLooping";
+    public const string CurrentCoin = "Coin";
+    public const string CurrentCash = "Cash";
 }
