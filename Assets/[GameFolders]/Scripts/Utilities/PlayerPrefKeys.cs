7ae4449e231362b9438b84fb0f2cd81e6688cc91
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2023-08-18 14:36:06 +0300

    ManagersAdded

---
+++ Assets/[GameFolders]/Scripts/Utilities/PlayerPrefKeys.cs
@@ -0,0 +1,6 @@
+public static class PlayerPrefKeys
+{
+    public static readonly string LastLevel = "LastLevel";
+    public static readonly string FakeLevel = "FakeLevel";
+    public static readonly string IsLooping = "IsLooping";
+}
